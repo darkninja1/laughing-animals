cd09f5795526292745e7d6dad18dd079ef0d3a18
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2022-01-12 03:01:45 +0000

    ...

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ var pic = ['pics/lep.png','pics/lio.jpg','pics/hyena.jpg','pics/mon.png','pics/l
 
 function laugh() {
   var random = Math.floor(Math.random() * file.length);
-  var x = Math.floor(Math.random() * 50);
-  var y = Math.floor(Math.random() * 50);
+  var x = Math.floor(Math.random() * 60);
+  var y = Math.floor(Math.random() * 60);
   var lol = document.createElement("AUDIO");
   var lol2 = document.createElement("IMG");
   lol2.setAttribute("src",pic[random]);
-  lol2.style.position = "relative";
+  lol2.style.position = "absolute";
   lol2.style.width = 'auto';
   lol2.style.height = '100px';
-  lol2.style.top = (y+25)+"%";
-  lol2.style.left = (x+25)+"%";
+  lol2.style.top = (y+5)+"%";
+  lol2.style.left = (x+5)+"%";
   lol.style.display = "none";
   lol.setAttribute("src",file[random]);
   document.body.appendChild(lol);
